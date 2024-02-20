76e0a7e048229f146ddd82f4493ab50f934f029e
itzDM <bitdev20@gmail.com>
2024-02-20 23:29:31 +0530

    fix : Deploy Error

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def validateUrl(url):
             return {"error":"Invalid Url"}
 
     except Exception as e:
-        return {"error":"Failed to fetch data, please try again later"}
+        return {"error":"Failed to fetch data, please try again later",
+                "message":e}
 def get_soup(url,company):    
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     req= requests.get(url,headers=headers)
